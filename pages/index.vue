a07fbeb2d18e24457a00a283f2519e80bf1448b6
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-11-29 17:18:24 -0300

    rm some stuff

--- pages/index.vue
+++ pages/index.vue
@@ -16,63 +16,13 @@
     <a href="https://github.com/BayBreezy/nuxt-ui-thing-starter" class="href">https://github.com/BayBreezy/nuxt-ui-thing-starter</a>
 
     <a href="https://github.com/ZTL-UwU/shadcn-docs-nuxt" class="href">https://github.com/ZTL-UwU/shadcn-docs-nuxt</a>
-    <p class="mt-2">
-      <strong>Nuxt 3</strong>
-      ,
-      <strong>Pinia</strong>
-      ,
-      <strong>Tailwind CSS</strong>
-      ,
-      <strong>VueUse</strong>
-      ,
-      <strong>NuxtImage</strong>
-      ,
-      <strong>shadcn-vue</strong>
-      ,
-      <strong>@radix-icons/vue</strong>
-    </p>
-
-    <ClientOnly>
-      <HoverCard>
-        <HoverCardTrigger>
-          <Button class="mt-4 rounded-full h-8 w-8 p-0">
-            <InfoCircledIcon class="rounded-full h-8 w-8"></InfoCircledIcon>
-          </Button>
-        </HoverCardTrigger>
-        <HoverCardContent class="text-center">
-          This is a combination of shadcn-vue's
-          <strong>HoverCard</strong>
-          &
-          <strong>Button</strong>
-          components.
-        </HoverCardContent>
-      </HoverCard>
-    </ClientOnly>
   </div>
 </template>
 
 <script setup>
-import {InfoCircledIcon} from "@radix-icons/vue";
-import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card";
 const colorMode = useColorMode();
 
 console.log(colorMode.preference);
 </script>
 
-<style lang="postcss" scoped>
-.active {
-  @apply bg-black text-white rounded-full dark:bg-white dark:text-black;
-}
-body {
-  background-color: #fff;
-  color: rgba(0, 0, 0, 0.8);
-}
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-.sepia-mode body {
-  background-color: #f1e7d0;
-  color: #433422;
-}
-</style>
+<style lang="postcss" scoped></style>
