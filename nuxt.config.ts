86e144501da3bd6290a3e3589e3f016d01656f59
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-11-30 19:35:26 -0300

    rm pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: {enabled: true},
-  modules: [
-    "@nuxt/image",
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-    "nuxt-posthog",
-    "@nuxt/icon",
-    "@nuxtjs/color-mode",
-    "@nuxt/content",
-  ],
+  modules: ["@nuxt/image", "@vueuse/nuxt", "@nuxtjs/tailwindcss", "shadcn-nuxt", "nuxt-posthog", "@nuxt/icon", "@nuxtjs/color-mode", "@nuxt/content"],
   colorMode: {
     preference: "system", // default value of $colorMode.preference
     fallback: "light", // fallback value if not system preference found
